87840185129d22b90d34bec97d96a9c59abe00b4
Joey Bodoia <jbodoia21@cmc.edu>
2020-12-14 14:17:02 -0800

    responsive styling changes

--- src/project.js
+++ src/project.js
@@ -15,8 +15,8 @@ const Project = (props) => {
     <h1>{props.name}</h1>
       <img className = "img shrink" src={props.img} alt={props.alt} onClick={props.open}/>
       <div className='Container '>
-        {frontend ? <a href={props.gitFront} target="_blank" className="shrink">Github Frontend</a>: (git ? <a href={props.git} target="_blank" className="shrink">Github</a>: null) }
-        {backend ? <a href={props.gitBack} target="_blank" className="shrink">Github Backend</a>: null }
+        {frontend ? <a id="githubCodex" href={props.gitFront} target="_blank" className="shrink">Github Frontend</a>: (git ? <a href={props.git} target="_blank" className="shrink">Github</a>: null) }
+        {backend ? <a id="githubCodex" href={props.gitBack} target="_blank" className="shrink">Github Backend</a>: null }
         <a href={props.live} target="_blank" className="shrink">App</a>
       </div>
       
