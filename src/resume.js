3005a6f4fbaf02598067fa774bb96b20f19c1e21
joeybodoia <jbodoia21@cmc.edu>
2022-06-24 18:01:43 -0700

    updated resume

--- src/resume.js
+++ src/resume.js
@@ -25,7 +25,7 @@ const ResumeModal = forwardRef((props, ref) => {
         <div className={'modal-wrapper'}>
             <div onClick={closeR} className={"modal-backdrop"}></div>
             <div className={"modal-box-resume"}>
-                    <iframe class="responsive-iframe" src="https://docs.google.com/document/d/e/2PACX-1vQOgBR3-ZnM9AWNY5Lq0XVWedr_6CVaRmZSVHiN1730gizFXrWV0N7pqVsNR22GiQ/pub?embedded=true" style={{'width':'80%', 'height':'100%',"font-size":"1vw"}}></iframe>
+                    <iframe class="responsive-iframe" src="https://docs.google.com/document/d/e/2PACX-1vQ8todwbsoj2_aaV3zwEwFU0hg2SKP22XIhEaGKRCYizodAnucACiwmn3i2WYtkzg/pub" style={{'width':'80%', 'height':'100%',"font-size":"1vw"}}></iframe>
                     {/* <iframe class="responsive-iframe" src="https://docs.google.com/document/d/e/2PACX-1vQOgBR3-ZnM9AWNY5Lq0XVWedr_6CVaRmZSVHiN1730gizFXrWV0N7pqVsNR22GiQ/pub?embedded=true" style={{'width':'80%', 'height':'100%',"font-size":"1vw"}}></iframe>     */}
                 <div>
                     <button onClick={closeR} className="closeButton">Close</button>
